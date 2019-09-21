15e3711b2df11cc6a807845792e5e81b2d58b5f0
Sai Ram Somanaboina <sai.somanaboina@myntra.com>
2019-09-21 18:59:53 +0530

    init

---
+++ src-trader-dashboard/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React from "react";
+import {render} from "react-dom";
+
+import {Provider} from "react-redux";
+
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-fresh.css";
+
+import StoreService from './services/StoreService';
+import TraderDashboard from "./components/TraderDashboard.jsx";
+
+let store = StoreService.STORE;
+
+document.addEventListener('DOMContentLoaded', () => {
+    render(
+        <Provider store={store}>
+            <TraderDashboard />
+        </Provider>,
+        document.querySelector('#traderDashboard')
+    );
+});
+
